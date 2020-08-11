d077cb0d2aada696d10d212466db836d901d3ee1
sarahsindet <sarahsindet@gmail.com>
2020-08-11 13:07:55 +0300

    register models to admin

--- instagramclone/admin.py
+++ instagramclone/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+from django.contrib import admin
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+
